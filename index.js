84cea0cd8feeefce2afc36faba66bf4c1366f884
Francis Brito <fr.br94@gmail.com>
2015-10-03 16:08:37 -0400

    Update `Test#end` logic to avoid creating extra object.

--- index.js
+++ index.js
@@ -4,23 +4,14 @@
 
 var request = require('supertest')
 
+var Test = request.Test
+
 if (!Promise) {
   throw new Error('Supertest-promise requires native promises or a polyfill.')
 }
 
-var fakeApp = {
-  address: function () {
-    return {
-      address: '0.0.0.0',
-      port: 0
-    }
-  },
-  listen: function () {}
-}
-
-var proto = Object.getPrototypeOf(request(fakeApp).get('/'))
-var end = proto.end
-proto.end = function (cb) {
+var end = Test.prototype.end
+Test.prototype.end = function (cb) {
   if (typeof cb === 'function') return end.apply(this, arguments)
 
   return new Promise(function (good, bad) {
